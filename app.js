cbab012a5efc2dbd8dc7a617ed47521c853d715b
abdallahaac <aceydesignz@gmail.com>
2022-10-23 13:40:26 -0400

    added animating dropdown

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const burgerMenuButton = document.querySelector(".burger-container");
+const burgerNavigationMenu = document.querySelector(".burger-navigation-menu");
 let isBurgerOpen = false;
 
 burgerMenuButton.addEventListener("click", () => {
 	if (!isBurgerOpen) {
 		burgerMenuButton.classList.add("expand");
+
+		burgerNavigationMenu.classList.add("expand");
+
 		isBurgerOpen = true;
 	} else {
 		burgerMenuButton.classList.remove("expand");
+
+		burgerNavigationMenu.classList.remove("expand");
+
 		isBurgerOpen = false;
 	}
 });
